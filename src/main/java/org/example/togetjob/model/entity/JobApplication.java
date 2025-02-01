f69441b2dc8282ece2e8a968fa1a043e461b9bb7
Gaia Meola <gaia.meola@gmail.com>
2025-02-01 17:52:52 +0100

    Settimo Commit: JobApplication

--- src/main/java/org/example/togetjob/model/entity/JobApplication.java
+++ src/main/java/org/example/togetjob/model/entity/JobApplication.java
@@ -7,26 +7,55 @@ public class JobApplication {
     private LocalDate applicationDate;
     private Student student;
     private Status status;
-    private JobApplication jobApplication ;
+    private String coverLetter;
+    private JobAnnouncement jobAnnouncement;
 
-    public JobApplication(LocalDate applicationDate, Student student, Status status ) {
-        this.applicationDate = applicationDate;
+
+    public JobApplication(Student student, String coverLetter, JobAnnouncement jobAnnouncement) {
+        this.applicationDate = LocalDate.now();
         this.student = student;
         this.status = Status.PENDING;
-
+        this.coverLetter = coverLetter;
+        this.jobAnnouncement = jobAnnouncement;
     }
 
     public LocalDate getApplicationDate() {
         return applicationDate;
     }
 
+    public void setApplicationDate(LocalDate applicationDate) {
+        this.applicationDate = applicationDate;
+    }
+
     public Student getStudent() {
         return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
     }
 
     public Status getStatus() {
         return status;
     }
 
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 
+    public String getCoverLetter() {
+        return coverLetter;
+    }
+
+    public void setCoverLetter(String coverLetter) {
+        this.coverLetter = coverLetter;
+    }
+
+    public JobAnnouncement getJobAnnouncement() {
+        return jobAnnouncement;
+    }
+
+    public void setJobAnnouncement(JobAnnouncement jobAnnouncement) {
+        this.jobAnnouncement = jobAnnouncement;
+    }
 }
