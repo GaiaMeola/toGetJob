1a3172404a8e9e1593a7ca7532502d49b42eecf7
Enrico Maria Talone <nbd3vie@gmail.com>
2025-02-16 23:09:38 +0100

    Commit

--- src/main/java/org/example/togetjob/testing/Testing.java
+++ src/main/java/org/example/togetjob/testing/Testing.java
@@ -1,31 +1,111 @@
 package org.example.togetjob.testing;
 
-import org.example.togetjob.bean.RegisterUserBean;
-import org.example.togetjob.exceptions.UsernameTakeException;
+import org.example.togetjob.config.AppConfig;
+import org.example.togetjob.connection.DatabaseConfig;
+import org.example.togetjob.dao.abstractfactorydao.AbstractFactoryDaoSingleton;
+import org.example.togetjob.dao.concreteobjects.DataBaseUserDao;
+import org.example.togetjob.exceptions.*;
+import org.example.togetjob.model.entity.Role;
+import org.example.togetjob.model.entity.Student;
 import org.example.togetjob.printer.Printer;
-import org.example.togetjob.view.boundary.RegisterBoundary;
+import org.example.togetjob.view.boundary.LoginBoundary;
 
-//username già presente ,email già presente,data errata, campi vuoti)
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class Testing {
 
-    private final RegisterBoundary registerBoundary = new RegisterBoundary();
+    private static final String STRING = "test successful";
 
-    public void testUsername(){
+    public static void main(String[] args) {
+        try {
+            AppConfig.loadConfigs();
+            DatabaseConfig databaseConfig = DatabaseConfig.getInstance();
+            databaseConfig.setConfigLoader(AppConfig.getDaoConfig());
+            try (Connection connection = databaseConfig.getConnection()) {
+                if (connection != null) {
+                    Printer.print("Database connection successful.");
+                } else {
+                    Printer.print("Error during connection.");
+                }
+            } catch (SQLException e) {
+                Printer.print("Error during connection: " + e.getMessage());
+            }
+        } catch (ConfigException e) {
+            Printer.print("error");
+        }
 
-       RegisterUserBean userBean = new RegisterUserBean();
-       userBean.setUsername("alreadyExisting");
-       userBean.setEmail("newEmail@gmail.com");
-       userBean.setName("Mario");
-       userBean.setSurname("Rossi");
-       userBean.setRole("Recruiter");
-       userBean.setPassword("password");
+        testNullConfigDaoLoader();
 
-       try {
-           boolean registrationSuccess = registerBoundary.registerUser(userBean, userBean);
-       } catch (UsernameTakeException e) {
-           Printer.print("test successful");
-       }
-   }
+        Testing testing = new Testing();
+        testing.runTests();
+    }
 
+    public void runTests() {
+        testUsername();
+        testEmail();
+        testLoginPassword();
+        testLoginUsername();
+        databaseTesting();
+    }
+
+    public void testUsername() {
+        try {
+            saveUser("alreadyExisting", "email@", Role.RECRUITER);
+        } catch (UsernameTakeException e) {
+            Printer.print(STRING);
+        }
+    }
+
+    public void testEmail() {
+        try {
+            saveUser("username", "alreadyExisting@gmail.com", Role.RECRUITER);
+        } catch (EmailAlreadyExistsException e) {
+            Printer.print(STRING);
+        }
+    }
+
+    public void testLoginPassword() {
+        LoginBoundary loginBoundary = new LoginBoundary();
+        String username = "Username";
+        String password = "WrongPassword";
+        try {
+            loginBoundary.login(username, password);
+        } catch (WrongPasswordException e) {
+            Printer.print(STRING);
+        }
+    }
+
+    public void testLoginUsername() {
+        LoginBoundary loginBoundary = new LoginBoundary();
+        String username = "WrongUsername";
+        String password = "Password";
+        try {
+            loginBoundary.login(username, password);
+        } catch (UserNotFoundException e) {
+            Printer.print(STRING);
+        }
+    }
+
+    public void databaseTesting() {
+        try {
+            saveUser("username", "email@", Role.RECRUITER);
+        } catch (DatabaseException e) {
+            Printer.print(STRING);
+        }
+    }
+
+    public static void testNullConfigDaoLoader() {
+        try {
+            AbstractFactoryDaoSingleton.getFactoryDao();
+        } catch (IllegalStateException e) {
+            Printer.print(STRING);
+        }
+    }
+
+    private void saveUser(String username, String email, Role role) throws UsernameTakeException, EmailAlreadyExistsException, DatabaseException {
+        Student user = new Student("name", "surname", username, email, "password", role);
+        DataBaseUserDao dataBaseUserDao = new DataBaseUserDao();
+        dataBaseUserDao.saveUser(user);
+    }
 }
